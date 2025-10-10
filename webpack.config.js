5b08a081ad6126e218ff8407d6ad74e3d6e918e6
Anna Semenova <annasemenova1909@gmail.com>
2025-10-10 18:02:56 +0300

    feat: add tours tabs section without JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,105 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+//
+// module.exports = {
+//   entry: './src/js/main.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'bundle.js',
+//     clean: true,
+//     publicPath: '/',
+//   },
+//   devServer: {
+//     static: './dist',
+//     open: true,
+//     hot: true,
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//     }),
+//     new MiniCssExtractPlugin({
+//       filename: 'style.css',
+//     }),
+//     new CopyWebpackPlugin({
+//       patterns: [
+//         {
+//           from: 'src/assets/images',
+//           to: 'assets/images',
+//           noErrorOnMissing: true
+//         },
+//         {
+//           from: 'src/assets/icons',
+//           to: 'assets/icons',
+//           noErrorOnMissing: true
+//         },
+//       ]
+//     })
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.html$/i,
+//         loader: 'html-loader',
+//         options: {
+//           sources: {
+//             list: [
+//               {
+//                 tag: 'img',
+//                 attribute: 'src',
+//                 type: 'src',
+//               },
+//               {
+//                 tag: 'img',
+//                 attribute: 'srcset',
+//                 type: 'srcset',
+//               },
+//               {
+//                 tag: 'link',
+//                 attribute: 'href',
+//                 type: 'src',
+//               },
+//             ]
+//           }
+//         }
+//       },
+//       {
+//         test: /\.scss$/i,
+//         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//         type: 'asset/resource',
+//         generator: {
+//           filename: (pathData) => {
+//             const relativePath = path.relative(path.resolve(__dirname, 'src'), pathData.filename);
+//
+//             if (relativePath.includes('/icons/') || relativePath.includes('\\icons\\')) {
+//               return 'assets/icons/[name][ext][query]';
+//             } else {
+//               return 'assets/images/[hash][ext][query]';
+//             }
+//           }
+//         }
+//       },
+//       {
+//         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+//         type: 'asset/resource',
+//         generator: {
+//           filename: 'assets/fonts/[hash][ext][query]'
+//         }
+//       },
+//     ],
+//   },
+//   mode: 'development',
+// };
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/main.js',
@@ -22,51 +121,35 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'style.css',
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets/images',
+          to: 'assets/images',
+          noErrorOnMissing: true
+        },
+      ]
+    })
   ],
   module: {
     rules: [
       {
         test: /\.html$/i,
         loader: 'html-loader',
-        options: {
-          sources: {
-            list: [
-              {
-                tag: 'img',
-                attribute: 'src',
-                type: 'src',
-              },
-              {
-                tag: 'img',
-                attribute: 'srcset',
-                type: 'srcset',
-              },
-              {
-                tag: 'link',
-                attribute: 'href',
-                type: 'src',
-              },
-            ]
-          }
-        }
       },
       {
         test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         type: 'asset/resource',
         generator: {
-          filename: (pathData) => {
-            const relativePath = path.relative(path.resolve(__dirname, 'src'), pathData.filename);
-
-            if (relativePath.includes('/icons/') || relativePath.includes('\\icons\\')) {
-              return 'assets/icons/[hash][ext][query]';
-            } else {
-              return 'assets/images/[hash][ext][query]';
-            }
-          }
+          filename: 'assets/images/[hash][ext][query]'
         }
       },
       {
@@ -76,7 +159,24 @@ module.exports = {
           filename: 'assets/fonts/[hash][ext][query]'
         }
       },
+      {
+        test: /\.svg$/i,
+        include: path.resolve(__dirname, 'src/assets/icons'),
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/icons/[name][ext][query]'
+        }
+      },
     ],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@images': path.resolve(__dirname, 'src/assets/images'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons'),
+      '@fonts': path.resolve(__dirname, 'src/assets/fonts'),
+    }
   },
   mode: 'development',
 };
