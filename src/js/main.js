f9fa16ec932671c4c612dd86919ec22df7f2c034
Anna Semenova <annasemenova1909@gmail.com>
2025-10-14 18:53:41 +0300

    feat: add JavaScript logic for tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
-
 import '../scss/main.scss';
 
 import HeaderSticky from './modules/HeaderSticky';
+import TabsCollection from "./modules/Tabs";
 
 document.addEventListener('DOMContentLoaded', () => {
   new HeaderSticky();
 });
+new TabsCollection();
